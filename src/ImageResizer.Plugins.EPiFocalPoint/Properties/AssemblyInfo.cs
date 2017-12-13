930b6b3264c64de4855f9f0b9a65c82d0a216b6f
Stephan Lonntorp <stephan.kvart@creuna.se>
2017-12-13 11:02:15 +0100

    Modified nuspec and assemblyinfo to remove Creuna trademark

--- src/ImageResizer.Plugins.EPiFocalPoint/Properties/AssemblyInfo.cs
+++ src/ImageResizer.Plugins.EPiFocalPoint/Properties/AssemblyInfo.cs
@@ -6,15 +6,15 @@ using EPiServer.PlugIn;
 
 [assembly: AssemblyTitle("Focal Point Cropping")]
 [assembly: AssemblyDescription("Focal point based image cropping for EPiServer using ImageResizing.NET")]
-[assembly: AssemblyCompany("Creuna AB")]
-[assembly: AssemblyCopyright("Copyright 2017 Creuna AB")]
+[assembly: AssemblyCompany("@defsteph")]
+[assembly: AssemblyCopyright("Copyright 2017")]
 [assembly: AssemblyProduct("ImageResizer.Plugins.EPiFocalPoint")]
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("ImageResizer.Plugins.EPiFocalPoint.Tests")]
 [assembly: Guid("4206e11f-3084-48b7-9ec3-203d2e5edbb2")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: PlugInSummary(MoreInfoUrl = "<A href='https://github.com/CreunaAB/EPiFocalPoint'>GitHub Repository</A>", License = LicensingMode.Freeware)]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: PlugInSummary(MoreInfoUrl = "<A href='https://github.com/defsteph/EPiFocalPoint'>GitHub Repository</A>", License = LicensingMode.Freeware)]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
